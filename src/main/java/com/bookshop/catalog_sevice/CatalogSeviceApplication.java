ede93b759469d1fb7e523b7cd61f402ed8d1f9a2
Valerie Fan <valeriefan1009@proton.me>
2024-11-13 18:51:13 +0800

    Initialize project

---
+++ src/main/java/com/bookshop/catalog_sevice/CatalogSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.bookshop.catalog_sevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogSeviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookshop/catalog_sevice/CatalogSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookshop.catalog_sevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
