2310f69df418f8699de9abb526c4b49d9e9da959
Valerie Fan <valeriefan1009@proton.me>
2024-11-13 18:59:28 +0800

    Implement service and controller

--- src/main/java/com/bookshop/catalogservice/CatalogSeviceApplication.java
+++ src/main/java/com/bookshop/catalogservice/CatalogSeviceApplication.java
@@ -1,4 +1,4 @@
-package com.bookshop.catalog_sevice;
+package com.bookshop.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/bookshop/catalogservice/domain/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.bookshop.catalogservice.domain;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+
+}
